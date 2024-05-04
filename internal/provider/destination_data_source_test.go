828ebd66e20d4dc1a0e0e20951eb63d7d7e1f402
Jose M. Tobar <jmtm12804@gmail.com>
2024-05-04 17:29:40 -0600

    updatd test names

--- internal/provider/destination_data_source_test.go
+++ internal/provider/destination_data_source_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-func TestAccDestinationDataSource(t *testing.T) {
+func TestDestinationDataSource(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
--- internal/provider/destinations_data_source_test.go
+++ internal/provider/destinations_data_source_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-func TestAccDestinationsDataSource(t *testing.T) {
+func TestDestinationsDataSource(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
